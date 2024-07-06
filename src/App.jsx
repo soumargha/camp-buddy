adbebf23d698c1e3a260f5131148f4c6e99c9852
Soumargha bhattacharjee <soumargha17@gmail.com>
2024-07-07 00:37:07 +0530

    initial comit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from './pages/LandingPage';
+
+function App() {
+ 
+
+  return (
+    <>
+     <Router>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+      </Routes>
+     </Router>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      
+<footer className="bg-zinc-50 text-center dark:bg-neutral-700 lg:text-left">
+  <div className="bg-black/5 p-4 text-center text-surface dark:text-white">
+    © 2023 Copyright:
+    <a href="https://tw-elements.com/">TW Elements</a>
+  </div>
+</footer>
+    </div>
+  )
+}
+
+export default Footer
