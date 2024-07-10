c7133d9721d246474db5a5ca74137479b5e870c0
Soumargha bhattacharjee <soumargha17@gmail.com>
2024-07-10 21:24:04 +0530

    commit

--- src/pages/Youritems.jsx
+++ src/pages/Youritems.jsx
@@ -4,7 +4,8 @@ import Table from '../components/Table'
 
 const Youritems = () => {
   return (
-    <><div className="flex justify-center mt-4">
+    <>
+    <div className="flex justify-center mt-4">
       <h1 className="text-2xl md:text-3xl pl-2 my-2 border-l-4 font-sans font-bold border-teal-400 text-center dark:text-black">
         Items you have bought
       </h1>
